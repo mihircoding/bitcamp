8ba316b8e05544366a25c099393f12f958fc8795
HaruV2 <sshmagic099@gmail.com>
2023-04-08 17:54:25 -0400

    updated picture taking and display

---
+++ PythonOCR/main.py
@@ -0,0 +1,4 @@
+from test import *
+
+def main():
+    data_init()
--- PythonOCR/test.py
+++ PythonOCR/test.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageFilter
-from scipy import ndimage
 import pytesseract
 import numpy as np
 import xlsxwriter
@@ -7,64 +6,108 @@ import cv2
 from cv2 import *
 from matplotlib import pyplot as plt
 from array import *
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-images = array('i', [])
-cam_port = 0
-cam = cv2.VideoCapture(cam_port)
-ctr = 0
-while(True):
-    _ , frame = cam.read()
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        print('escape hit, closing the app')
-        cv2.destroyAllWindows()
-        break
-    elif k%256 == 32:
-        picture = cv2.imwrite("captured" + str(ctr) + ".jpg", frame)
-        images.append(picture)
-        print('Screenshot taken')
-        ctr += 1
-        
-while True:
+directory = r'C:/Users/sshma/BitCamp/bitcamp/screenshots'
+workbook = xlsxwriter.Workbook('data.xlsx')
+images = []
+
+
+#Takes pictures and stores them in a folder until user stops
+def take_picture():
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+    ctr = 0
+    while(True):
+        os.chdir(directory)
+        _ , frame = cam.read()
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            print('escape hit, closing the app')
+            cv2.destroyAllWindows()
+            break
+        elif k%256 == 32:
+            picture = cv2.imwrite("captured" + str(ctr) + ".jpg", frame)
+            print('Screenshot taken')
+            ctr += 1
+        elif k%256 == 114: #removes the last picture taken
+            os.remove(os.path.join(directory, "captured" + str(ctr - 1) + ".jpg"))
+            ctr -= 1
+
+#Displays all the images taken
+def display():
+    images = [cv2.imread(file) for file in os.listdir(directory)]
     i = 0
-    display = cv2.imread(str(images[i]))
-    display = cv2.imshow('image', images[i])
-    cv2.waitKey(0)
-    k = cv2.waitKey(1)
-    if k%256 == 37:
-        if i == 0:
+    while True:
+        cv2.imshow("IMG" + str(i), images[i])
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            print('escape hit, closing the app')
+            cv2.destroyAllWindows()
             break
-        i -= 1
-        cv2.imshow('image', images[i])
-    if k%256 == 39:
-        if i == len(images):
-            break
-        i += 1
-        cv2.imshow('image', images[i])
-    if k%256 == 27:
-        print('escape hit, closing the app')
-        cam.destroyAllWindows()
-        break
+        elif k%256 == 32:
+            cv2.destroyWindow("IMG" + str(i))
+            if not i == len(images) - 1:
+                i += 1
+                cv2.imshow("IMG" + str(i), images[i])
+
+#Scans all the images that was taken
+def scan(images):
+    for image in images:
+        scan(image)
         
 
-def clear(image):
-    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    return cv2.filter2D(image, -1, kernel)
-    
-    
-filename2 = 'full.jpg'
-filename3 = 'receipt1.jpg'
-full = cv2.imread(filename3, cv2.IMREAD_GRAYSCALE)
-if (0):
-    h, w, _ = full.shape
-    full = full[int(h/5):int(h/2), 0:w]
-    full = cv2.resize(full, dsize = (800, 800), interpolation = cv2. INTER_LINEAR)
-    full = clear(full)
-    full = Image.fromarray(full, "RGB")
+def scan(image):
+    if (0):
+        h, w, _ = full.shape
+        full = full[int(h/5):int(h/2), 0:w]
+        full = cv2.resize(full, dsize = (800, 800), interpolation = cv2. INTER_LINEAR)
+        full = clear(full)
+        full = Image.fromarray(full, "RGB")
+    full = test(full)
 
+    cv2.imshow('image',full)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def read(image, workbook):
+    worksheet = data_init(workbook)
+    cost = workbook.add_format({'num_format': '$##.##'})
+    cost0 = workbook.add_format({'num_format': '$##.00'})
+    text = pytesseract.image_to_string(image)
+    if text == "":
+        print("Unable to Read Text. Please retake this image")
+    else:
+        text = text.replace('\n', ' ')
+    
+    index = 0
+    item_number = 1
+    row = 2
+    item = ''
+
+    print(text)
+    
+    split = text.split()
+    
+    for i in split:
+        if i.isalpha() or not "." in i:
+            item += i + ' '
+        elif "." in i:
+            dec = i.find('.')
+            price = int(i[:dec]) + (int(i[dec + 1:])/100)
+            worksheet.write(row, 0, item_number)
+            worksheet.write(row, 1, item)
+            if (int(i[dec + 1:])/100) == 0:
+                worksheet.write(row, 2, price, cost0)
+            else:
+                worksheet.write(row, 2, price, cost)
+            item = ''
+            row += 1
+            item_number += 1
+    
 def test(image):
     if(1):
         _ , image = cv2.threshold(image, 160, 255, cv2.THRESH_BINARY)
@@ -72,48 +115,26 @@ def test(image):
             image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
             image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     return image
-    
-full = test(full)
 
-cv2.imshow('image',full)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#Initializes the excel sheet that holds the scanned data
+def data_init(workbook):
+    worksheet = workbook.add_worksheet()
+    worksheet.write('A1', 'Item #')
+    worksheet.write('B1', 'Item Name')
+    worksheet.write('C1', 'Price')
+    worksheet.write('D1', 'Item Type') #user input?
+    return worksheet
 
-scanned = pytesseract.image_to_string(full)
-if scanned == "":
-    print("Please take a clearer image")
-else:
-    scanned = scanned.replace('\n', ' ')
+#Deletes all the photos taken
+def finish():
+    workbook.close()
+    for image in os.listdir(directory):
+        os.remove(os.path.join(directory, image))
+        
+def main():
+    data_init(workbook)
+    take_picture()
+    display()
+    finish()
 
-print(scanned)
-index = 0
-item_number = 1
-row = 2
-item = ''
-
-workbook = xlsxwriter.Workbook('hello.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'Item #')
-worksheet.write('B1', 'Item Name')
-worksheet.write('C1', 'Price')
-worksheet.write('D1', 'Item Type') #user input?
-
-cost = workbook.add_format({'num_format': '$##.##'})
-cost0 = workbook.add_format({'num_format': '$##.00'})
-split = scanned.split()
-for i in split:
-    if i.isalpha() or not "." in i:
-        item += i + ' '
-    if "." in i:
-        dec = i.find('.')
-        price = int(i[:dec]) + (int(i[dec + 1:])/100)
-        worksheet.write(row, 0, item_number)
-        worksheet.write(row, 1, item)
-        if (int(i[dec + 1:])/100) == 0:
-            worksheet.write(row, 2, price, cost0)
-        else:
-            worksheet.write(row, 2, price, cost)
-        item = ''
-        row += 1
-        item_number += 1
-workbook.close()
+main()
